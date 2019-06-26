9a6363d93478b1eea07751a8d211fd1de9d4ca75
Femi Balogun <phembarl@yahoo.com>
2019-06-26 15:53:34 +0100

    chore(testing): setup unit testing - install mocha - install chai and chai-http - configure Babel - write tests for implemented functionalities - create pull request template - Finishes #166841002

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import cors from 'cors';
 import errorhandler from 'errorhandler';
+import config from './db/config/config';
 
-const isProduction = process.env.NODE_ENV === "production";
+const { isProduction, port } = config;
 
 // Create global app object
 const app = express();
@@ -12,74 +13,70 @@ const app = express();
 app.use(cors());
 
 // Normal express config defaults
-app.use(require("morgan")("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require("method-override")());
-app.use(express.static(__dirname + "/public"));
-
 app.use(
-    session({
-        secret: "authorshaven",
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false
-    })
+  session({
+    secret: 'authorshaven',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
 );
 
 if (!isProduction) {
-    app.use(errorhandler());
+  app.use(errorhandler());
 }
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 200,
-        message: 'Welcome to Author\'s Haven',
-    })
-})
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+  res.status(200).json({
+    status: 200,
+    message: 'Welcome to Author\'s Haven',
+  });
 });
 
-/// error handlers
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
 
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-    app.use(function(err, req, res, next) {
-        console.log(err.stack);
+  app.use((err, req, res) => {
+    console.log(err.stack);
 
-        res.status(err.status || 500);
+    res.status(err.status || 500);
 
-        res.json({
-            errors: {
-                message: err.message,
-                error: err
-            }
-        });
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        errors: {
-            message: err.message,
-            error: {}
-        }
-    });
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
 });
 
 // finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("Listening on port " + server.address().port);
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${server.address().port}`);
 });
 
 export default server;
--- test/index.test.js
+++ test/index.test.js
@@ -6,11 +6,11 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('server', () => {
-    it('should start the server successfully', (done) => {
-        chai.request(app).get('/')
-        .end((err, res) => {
-            res.should.have.status(200);
-            done();
-        });
-    });
+  it('should start the server successfully', (done) => {
+    chai.request(app).get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 });
