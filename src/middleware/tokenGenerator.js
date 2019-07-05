a3e3323c67769498f5caceaed42a6a58d410aa7a
cavdy <cavdyofficials@gmail.com>
2019-07-05 12:06:22 +0100

    feature(user password reset):user password reset user can reset their password when they forget their password [finishes #166841011]

--- src/middleware/tokenGenerator.js
+++ src/middleware/tokenGenerator.js
@@ -1,11 +1,5 @@
 import jwt from 'jsonwebtoken';
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-
-dotenv.config();
-=======
 import { GENERATE_SECRET } from '../db/config/config';
->>>>>>> feature(user password reset):user password reset
 
 /**
  *
@@ -25,25 +19,16 @@ class TokenGenerator {
     const payload = {
       token: 'token'
     };
-<<<<<<< HEAD
-    jwt.sign(payload,
-      process.env.GENERATE_SECRET, {
-=======
     jwt.sign(
       payload,
       GENERATE_SECRET, {
->>>>>>> feature(user password reset):user password reset
         expiresIn: '1h'
       },
       async (err, token) => {
         req.generate = token;
         return next();
-<<<<<<< HEAD
-      });
-=======
       }
     );
->>>>>>> feature(user password reset):user password reset
   }
 
   /**
@@ -55,14 +40,9 @@ class TokenGenerator {
    * @memberof TokenGenerator
    */
   static verifyToken(req, res, next) {
-<<<<<<< HEAD
-    jwt.verify(req.query.resetToken,
-      process.env.GENERATE_SECRET,
-=======
     jwt.verify(
       req.query.resetToken,
       GENERATE_SECRET,
->>>>>>> feature(user password reset):user password reset
       (err, authorizedData) => {
         if (err) {
           return res.status(400).json({
@@ -72,12 +52,8 @@ class TokenGenerator {
         }
         req.authorizedData = authorizedData;
         return next();
-<<<<<<< HEAD
-      });
-=======
       }
     );
->>>>>>> feature(user password reset):user password reset
   }
 }
 
